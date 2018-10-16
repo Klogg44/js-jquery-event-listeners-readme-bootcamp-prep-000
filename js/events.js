a6c3fc35b69a1a06597ea3a7a6234546d7a4a724
Spencer Croy <spencerccroy@gmail.com>
2018-10-16 21:01:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -16,8 +16,13 @@ function frameIt() {
 }
 
 function pressIt() {
-
-  return;
+  $(input#typing).on('keydown',
+  function(key){
+    if (key.which == 71){
+      alert('G was pressed');
+    }
+  });
+  return;  
 }
 
 $(document).ready(function(){
